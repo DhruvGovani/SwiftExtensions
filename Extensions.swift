bd672343347ca4d8e727a090666229ce203b21a7
Dhruv Govani <dhruvgovani@icloud.com>
2020-06-27 13:28:27 +0530

    Update Extensions.swift

--- Extensions.swift
+++ Extensions.swift
@@ -4,13 +4,18 @@
 //
 //  Created by Dhruv Govani on 27/06/20.
 //
+//  Copyright (c) 2020 Dhruv Govani
+//
+//  MIT License : https://github.com/DhruvGovani/SwiftExtensions/blob/master/LICENSE
+//  GIT : https://github.com/DhruvGovani/SwiftExtensions
 
 import Foundation
 import UIKit
 
-///Round the edges of all views defined in parameter
-///Usage : UIView().RoundEdgesOf(Views: [Label,TextField,Button,SubView])
+
 extension UIView{
+    ///Round the edges of all views defined in parameter
+    ///Usage : UIView().RoundEdgesOf(Views: [Label,TextField,Button,SubView])
     func RoundEdgesOf(Views : [UIView]){
         
         for i in Views{
